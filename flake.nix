f11ab89ea46d099deb710fd3371cebc58469a07a
Prad Nukala <prad@sonr.io>
2025-10-03 15:08:42 -0400

    fix: ambiguous packages

--- flake.nix
+++ flake.nix
@@ -6,9 +6,12 @@
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
     {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          allPackages = import ./default.nix { inherit pkgs; };
+        in
+        nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) allPackages
+      );
     };
 }
